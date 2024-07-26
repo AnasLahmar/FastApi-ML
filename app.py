64214748f75603b521c9be02af14b5742fcaafe5
LAHMAR Anas <anas.lahmar@vinci-energies.net>
2024-07-26 09:49:06 +0200

    hello function

--- app.py
+++ app.py
@@ -12,6 +12,10 @@ async def root():
 async def test():
     return {"message": "cool !"}
 
+@app.get("/hello")
+async def test():
+    return {"message": "Beeee freeeeeeeeeeeee !"}
+
 # Supprimez ou commentez le bloc suivant dans votre fichier app.py
 #if __name__ == "__main__":
 #    uvicorn.run(app, host="localhost", port=8003)
